0682cc756aaf9976633cb46d25a226062fc28b81
Himesh Parashar <himeshparashar424@gmail.com>
2024-05-06 10:41:52 +0000

    corrected import

--- qa_toolkit.py
+++ qa_toolkit.py
@@ -1,8 +1,7 @@
 from abc import ABC
 from superagi.tools.base_tool import BaseToolkit, BaseTool
 from typing import Type, List
-from cypress_tool import CypressTool
-
+from superagi.tools.external_tools.cypress_tool.cypress_tool import CypressTool
 
 class QAToolkit(BaseToolkit, ABC):
     name: str = "QA agent Toolkit"
