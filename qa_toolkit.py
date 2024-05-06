4050760db3a446ccbd49e0c5a81a268caa4abaed
Himesh Parashar <himeshparashar424@gmail.com>
2024-05-06 09:52:15 +0000

    qa toolkit boilerplate made

---
+++ qa_toolkit.py
@@ -0,0 +1,15 @@
+from abc import ABC
+from superagi.tools.base_tool import BaseToolkit, BaseTool
+from typing import Type, List
+from cypress_tool import CypressTool
+
+
+class QAToolkit(BaseToolkit, ABC):
+    name: str = "QA agent Toolkit"
+    description: str = "QA agent toolkit contains all tools related to testing"
+
+    def get_tools(self) -> List[BaseTool]:
+        return [CypressTool()]
+
+    # def get_env_keys(self) -> List[str]:
+    #     return ["FROM"]
